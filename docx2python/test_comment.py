66ca261198f3090553de1b15e54d7c3880d0d052
David Ayala <deayalar@gmail.com>
2023-09-28 15:18:53 -0500

    Remove test file

--- docx2python/test_comment.py
+++
@@ -1,8 +0,0 @@
-from docx2python import docx2python
-
-file_path = "/home/deayalar/Downloads/Contract ID 58605 - Immunoprecise Antibodies -  - Master Services Agreement (MSA)_IPAedits2.8.2023--v1 (2).docx"
-file_path = "/home/deayalar/Downloads/Mock MSA counterparty edits for comparison.docx"
-
-with docx2python(file_path, extract_comments=True) as docx_content:
-    with open("docx2python.txt", "w+") as txt_file:
-        txt_file.write(docx_content.text)
