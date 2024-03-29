c06a271934a0f8f492b32aa79b611e4a3514202c
Shay Hill <shay_public@hotmail.com>
2024-03-29 14:14:15 -0500

    test: add test file and test stub for comments

---
+++ tests/test_comments.py
@@ -0,0 +1,19 @@
+"""Test extracting comments.
+
+User flyguy62n requested comment extraction. Extract comments as tuples (text,
+author, date, comment).
+
+:author: Shay Hill
+:created: 2024-03-29
+"""
+
+from docx2python import docx2python
+
+from .conftest import RESOURCES
+
+
+def test_comments() -> None:
+    """Extract comments and some comment metadata."""
+    pars = docx2python(RESOURCES / "comments.docx")
+    pars.close()
+    assert True
